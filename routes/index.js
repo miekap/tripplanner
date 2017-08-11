a7ab7594e5a96dc1699cad1bc9dc502c7667defa
Anna Chen <xqchen91@gmail.com>
2017-08-11 17:02:31 -0400

    attempt at routing in everything on the database

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 const router = require('express').Router()
+const db = require('./models')
+const hotels = db.hotel;
+const activities = db.models.activity;
+const restaurants = db.models.restaurant;
 
 router.get('/', function(req, res, next){
-	res.render('index')
+	let h = hotels.findAll()
+	let a = activities.findAll()
+	let r = restaurants.findAll()
+	res.render('index', {hotels:h,activities:a,restaurants:r})
 })
 
 router.get('/error', function(req, res, next){
