f97f5b651de8e3d70e15e40072e9c304a1d35095
Anna Chen <xqchen91@gmail.com>
2017-08-15 10:21:15 -0400

    home page works

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,32 @@ const activities = db.models.activity;
 const restaurants = db.models.restaurant;
 
 router.get('/', function(req, res, next){
-	let h = hotels.findAll()
-	let a = activities.findAll()
-	let r = restaurants.findAll()
-	let har = Promise.all([h,a,r])
-	res.render('index', {hotels:har[0],activities:har[1],restaurants:har[2]})
+	let h, a, r
+
+	// let h = hotels.findAll()
+	// let a = activities.findAll()
+	// let r = restaurants.findAll()
+	// Promise.all([h,a,r])
+	// 	.then(function(har){
+	// 		res.render(index, {hotels:har[0],activities:har[1],restaurants:har[2]})
+	// 	})
+
+	hotels.findAll()
+		.then(function(hotels){
+			h = hotels;
+			return activities.findAll()
+		})
+		.then(function(acts){
+			a = acts;
+			return restaurants.findAll()
+		})
+		.then(function(res){
+			r=res;
+		})
+		.then(function(){
+			res.render('index', {hotels:h,activities:a,restaurants:r})
+		})
+	// res.render('index', {hotels:har[0],activities:har[1],restaurants:har[2]})
 })
 
 router.get('/error', function(req, res, next){
